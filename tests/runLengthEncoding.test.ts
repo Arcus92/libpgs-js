728e0345c50c875d3a736de975689dc47b915736
David Schulte <mail@david-schulte.de>
2024-07-09 21:56:42 +0200

    Improve the run-length-encoding performance: - Avoid using slow callbacks - Copy and storing the rgba value as one 32bit value instead of four single bytes. - Store rgba data in one buffer and not objects

--- tests/runLengthEncoding.test.ts
+++ tests/runLengthEncoding.test.ts
@@ -5,30 +5,28 @@ test('2x2 run length decode with linebreak', () => {
         0x01 /* raw byte */, 0x02 /* raw byte */, 0x00, 0x00 /* line break */,
         0x03 /* raw byte */, 0x04 /* raw byte */
     ]);
-    const result: {idx: number, x: number, y: number, value: number}[] = [];
-    RunLengthEncoding.decode(data, (idx, x, y, value) => {
-        result.push({ idx, x, y, value });
-    });
+    const map = [0x00, 0x01, 0x02, 0x03, 0x04];
+    const output = new Uint8Array(4);
+    const length = RunLengthEncoding.decode(data, map, output);
 
-    expect(result.length).toBe(4);
-    expect(result[0]).toMatchObject({ idx: 0, x: 0, y: 0, value: 0x01 });
-    expect(result[1]).toMatchObject({ idx: 1, x: 1, y: 0, value: 0x02 });
-    expect(result[2]).toMatchObject({ idx: 2, x: 0, y: 1, value: 0x03 });
-    expect(result[3]).toMatchObject({ idx: 3, x: 1, y: 1, value: 0x04 });
+    expect(length).toBe(4);
+    expect(output[0]).toBe(0x01);
+    expect(output[1]).toBe(0x02);
+    expect(output[2]).toBe(0x03);
+    expect(output[3]).toBe(0x04);
 });
 
 test('10x1 run length decode with repeating null-byte', () => {
     const data = new Uint8Array([
         0x00, 0x08 /* count */
     ]);
-    const result: {idx: number, x: number, y: number, value: number}[] = [];
-    RunLengthEncoding.decode(data, (idx, x, y, value) => {
-        result.push({ idx, x, y, value });
-    });
+    const map = [0x00];
+    const output = new Uint8Array(8);
+    const length = RunLengthEncoding.decode(data, map, output);
 
-    expect(result.length).toBe(8);
+    expect(length).toBe(8);
     for (let i = 0; i < 8; i++) {
-        expect(result[i]).toMatchObject({ idx: i, x: i, y: 0, value: 0x00 });
+        expect(output[i]).toBe(0x00);
     }
 });
 
@@ -36,14 +34,13 @@ test('10x1 run length decode with repeating byte', () => {
     const data = new Uint8Array([
         0x00, 0x08 /* 6bit count */ | (1 << 7) /* not null byte */, 0x01 /* repeated byte */
     ]);
-    const result: {idx: number, x: number, y: number, value: number}[] = [];
-    RunLengthEncoding.decode(data, (idx, x, y, value) => {
-        result.push({ idx, x, y, value });
-    });
+    const map = [0x00, 0x01];
+    const output = new Uint8Array(8);
+    const length = RunLengthEncoding.decode(data, map, output);
 
-    expect(result.length).toBe(8);
+    expect(length).toBe(8);
     for (let i = 0; i < 8; i++) {
-        expect(result[i]).toMatchObject({ idx: i, x: i, y: 0, value: 0x01 });
+        expect(output[i]).toBe(0x01);
     }
 });
 
@@ -51,14 +48,13 @@ test('522x1 run length decode with repeating null byte', () => {
     const data = new Uint8Array([
         0x00, 0x02 /* higher 6bit count (2x256) */ | (1 << 6) /* 14bit count */, 0x0A /* lower 8bit count (10) */
     ]);
-    const result: {idx: number, x: number, y: number, value: number}[] = [];
-    RunLengthEncoding.decode(data, (idx, x, y, value) => {
-        result.push({ idx, x, y, value });
-    });
+    const map = [0x00];
+    const output = new Uint8Array(522);
+    const length = RunLengthEncoding.decode(data, map, output);
 
-    expect(result.length).toBe(522);
+    expect(length).toBe(522);
     for (let i = 0; i < 522; i++) {
-        expect(result[i]).toMatchObject({ idx: i, x: i, y: 0, value: 0x00 });
+        expect(output[i]).toBe(0x00);
     }
 });
 
@@ -67,13 +63,12 @@ test('522x1 run length decode with repeating byte', () => {
     const data = new Uint8Array([
         0x00, 0x02 /* higher 6bit count (2x256) */ | (1 << 7) /* not null byte */ | (1 << 6) /* 14bit count */, 0x0A /* lower 8bit count (10) */, 0x01 /* repeated byte */
     ]);
-    const result: {idx: number, x: number, y: number, value: number}[] = [];
-    RunLengthEncoding.decode(data, (idx, x, y, value) => {
-        result.push({ idx, x, y, value });
-    });
+    const map = [0x00, 0x01];
+    const output = new Uint8Array(522);
+    const length = RunLengthEncoding.decode(data, map, output);
 
-    expect(result.length).toBe(522);
+    expect(length).toBe(522);
     for (let i = 0; i < 522; i++) {
-        expect(result[i]).toMatchObject({ idx: i, x: i, y: 0, value: 0x01 });
+        expect(output[i]).toBe(0x01);
     }
 });
